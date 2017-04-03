c4f23619c9f70ad39ae29268f02852e7d3e0f47b
Artjom31415 <artjom31415@googlemail.com>
2017-04-03 18:20:48 +0200

    Make function fail on non-sense input and add option for exact y_position

--- R/significance_annotation.R
+++ R/significance_annotation.R
@@ -3,12 +3,18 @@
 SignifAnnot <- ggplot2::ggproto("SignifAnnot", ggplot2::Stat,
                   required_aes = c("x", "y", "group"),
                   setup_params = function(data, params) {
+                    if(any(data$group == -1)|| any(data$group != data$x)){
+                      stop("Can only handle data with groups that are plotted on the x-axis")
+                    }
                     if (is.character(params$test)) params$test <- match.fun(params$test)
                     params$complete_data <- data
+                    if(! is.null(params$y_position) && length(params$y_position) == 1)
+                      params$y_position <- rep(params$y_position, length(params$comparisons))
                     if(length(params$margin_top) == 1) params$margin_top <- rep(params$margin_top, length(params$comparisons))
                     if(length(params$step_increase) == 1) params$step_increase <- rep(params$step_increase, length(params$comparisons))
                     if(length(params$tip_length) == 1) params$tip_length <- rep(params$tip_length, length(params$comparisons))
-                    if(! is.null(params$annotations)) params$annotations <- rep(params$annotations, length(params$comparisons))
+                    if(! is.null(params$annotations) && length(params$annotations) == 1)
+                      params$annotations <- rep(params$annotations, length(params$comparisons))
 
                     if(params$map_signif_level == TRUE){
                       params$map_signif_level <- c("***"=0.001, "**"=0.01, "*"=0.05)
@@ -23,8 +29,8 @@ SignifAnnot <- ggplot2::ggproto("SignifAnnot", ggplot2::Stat,
                     }
                     return(params)
                   },
-                  compute_group = function(data, scales, comparisons, test, complete_data,
-                                           annotations, map_signif_level,
+                  compute_group = function(data, scales, comparisons, test, test.args, complete_data,
+                                           annotations, map_signif_level, y_position,
                                            margin_top, step_increase, tip_length) {
                     i <- 0
                     result <- lapply(comparisons, function(comp){
@@ -34,7 +40,7 @@ SignifAnnot <- ggplot2::ggproto("SignifAnnot", ggplot2::Stat,
                         test_result <- if(is.null(annotations)){
                           group_1 <- complete_data$y[complete_data$x == scales$x$map(comp[1]) & complete_data$PANEL == data$PANEL[1]]
                           group_2 <- complete_data$y[complete_data$x == scales$x$map(comp[2]) & complete_data$PANEL == data$PANEL[1]]
-                          p_value <- do.call(test, list(group_1, group_2))$p.value
+                          p_value <- do.call(test, c(list(group_1, group_2), test.args))$p.value
                           if(is.numeric(map_signif_level)){
                             temp_value <- names(which.min(map_signif_level[which(map_signif_level > p_value)]))
                             if(is.null(temp_value)){
@@ -48,9 +54,12 @@ SignifAnnot <- ggplot2::ggproto("SignifAnnot", ggplot2::Stat,
                         }else{
                           annotations[i]
                         }
-                        # print(test_result)
                         y_scale_range <- (scales$y$range$range[2] - scales$y$range$range[1])
-                        y_pos <- scales$y$range$range[2] + y_scale_range * margin_top[i] + y_scale_range * step_increase[i] * (i-1)
+                        y_pos <- if(is.null(y_position)){
+                          scales$y$range$range[2] + y_scale_range * margin_top[i] + y_scale_range * step_increase[i] * (i-1)
+                        }else{
+                          y_pos <- y_position[i]
+                        }
                         data.frame(x=c(min(comp[1],comp[2]),min(comp[1],comp[2]),max(comp[1],comp[2])),
                                    xend=c(min(comp[1],comp[2]),max(comp[1],comp[2]),max(comp[1],comp[2])),
                                    y=c(y_pos - y_scale_range*tip_length[i], y_pos, y_pos),
@@ -65,18 +74,17 @@ SignifAnnot <- ggplot2::ggproto("SignifAnnot", ggplot2::Stat,
 
 stat_signif <- function(mapping = NULL, data = NULL,
                     position = "identity", na.rm = FALSE, show.legend = NA,
-                    inherit.aes = TRUE, comparisons=NULL, test="wilcox.test",
-                    annotations=NULL, map_signif_level=FALSE,
+                    inherit.aes = TRUE, comparisons=NULL, test="wilcox.test", test.args=NULL,
+                    annotations=NULL, map_signif_level=FALSE,y_position=NULL,
                     margin_top=0.05, step_increase=0.02, tip_length=0.03,
                     ...) {
-  # if(interactive()) browser()
   ggplot2::layer(
     stat = SignifAnnot, data = data, mapping = mapping, geom = "signif",
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(comparisons=comparisons, test=test, na.rm = na.rm,
-                  annotations=annotations, map_signif_level=map_signif_level,
+    params = list(comparisons=comparisons, test=test, test.args=test.args,
+                  annotations=annotations, map_signif_level=map_signif_level,y_position=y_position,
                   margin_top=margin_top, step_increase=step_increase,
-                  tip_length=tip_length, ...)
+                  tip_length=tip_length, na.rm = na.rm, ...)
   )
 }
 
--- tests/testthat/test-significance_annotation.R
+++ tests/testthat/test-significance_annotation.R
@@ -7,10 +7,21 @@ test_that("the plotting works", {
     ggplot(mpg, aes(x=manufacturer, y=displ)) +
       geom_boxplot()  +
       stat_signif(comparisons=list(c("audi", "ford"), c("hyundai", "nissan")),
-                  # annotations=c("Interesting", "Too far apart"),
-                  map_signif_level=TRUE,
-                  test=t.test, vjust=-0.1, margin_top=0.02, step_increase=0, tip_length=0.01) +
+                  annotations=c("Interesting", "Too far apart"),
+                  # map_signif_level=TRUE,
+                  test="wilcox.test", test.args=list(alternative="two.sided"),
+                  vjust=-0.1, margin_top=0.02, step_increase=0, tip_length=0.01) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       facet_wrap(~ as.factor(year), scale="free")
   )
+})
+
+
+test_that("non-sense fails", {
+  skip("for now...")
+  expect_error(print(
+    ggplot(mpg, aes(y=hwy, x=displ, group=manufacturer)) +
+      geom_point()  +
+      stat_signif(comparisons=list(c("audi", "ford"), c("hyundai", "nissan")))
+  ))
 })
