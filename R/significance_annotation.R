8974b60d56d66835dcb480b45806f5f9fe90792f
Artjom31415 <artjom31415@googlemail.com>
2017-04-04 14:06:12 +0200

    Add documentation to fix warnings during check

--- R/significance_annotation.R
+++ R/significance_annotation.R
@@ -94,6 +94,13 @@ StatSignif <- ggplot2::ggproto("StatSignif", ggplot2::Stat,
 #' @param step_increase numeric vector with the increase in fraction of total height for every additional comparison to
 #'   minimize overlap.
 #' @param tip_length numeric vector with the fraction of total height that the bar goes down to indicate the precise column
+#' @param na.rm If \code{FALSE} (the default), removes missing values with
+#'    a warning.  If \code{TRUE} silently removes missing values.
+#' @param ... other arguments passed on to \code{\link{layer}}. These are
+#'   often aesthetics, used to set an aesthetic to a fixed value, like
+#'   \code{color = "red"} or \code{size = 3}. They may also be parameters
+#'   to the paired geom/stat.
+#' @inheritParams ggplot2::layer
 #' @examples
 #' library(ggplot2)
 #' ggplot(mpg, aes(class, hwy)) +
